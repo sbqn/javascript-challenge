b5c275cbd21548b185b0a35f0064e085d8ac2465
sbqn <88091465+sbqn@users.noreply.github.com>
2021-08-10 19:56:49 +0700

    Update CaseStudy1.js
    casestudy1 sudah dikerjakan

--- Day 1/CaseStudy1.js
+++ Day 1/CaseStudy1.js
@@ -2,3 +2,13 @@ var x = 5;
 var y = 8.5;
 
 //buatlah proses perhitungan penjumlahan, pengurangan, perkalian, dan pembagian dari kedua variabel di atas.
+
+var penjumlahan = x + y;
+var pengurangan = x - y;
+var perkalian = x * y;
+var pembagian = x / y;
+
+document.write(penjumlahan);
+document.write(pengurangan);
+document.write(perkalian);
+document.write(pembagian);
